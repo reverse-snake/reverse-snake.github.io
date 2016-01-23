bdc18d55c4b499cd04396a8c61a9e448eba514c5
jbzdarkid <jbzdarkid@gmail.com>
2016-01-23 08:12:21 -0500

    off by 1

--- Snake.js
+++ Snake.js
@@ -17,7 +17,7 @@ function stopGame() {
     board[i].length = height;
   }
   snake = [];
-  for (var i=0; i<level+2; i++) {
+  for (var i=0; i<level+3; i++) {
     snake.unshift([i, 0]);
   }
   debugSnake();
