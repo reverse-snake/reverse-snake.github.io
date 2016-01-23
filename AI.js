6033fe52a9c679eede1c5f0a8df513896c1dc81d
Maddy Longo <mclongovt@gmail.com>
2016-01-23 11:54:36 -0500

    Added button debugging

--- AI.js
+++ AI.js
@@ -1,6 +1,7 @@
 var dir = 'right';
 var level = -1; // Since startGame() increases level, and our first AI is 0, we start at -1.
 var maxLevel = 0;
+console.log(document.getElementsByClassName("upButton"));
 var upButton = document.getElementsByClassName("upButton")[0];
 var levelGauge = document.getElementsByClassName("level")[0];
 var downButton = document.getElementsByClassName("downButton")[0];
