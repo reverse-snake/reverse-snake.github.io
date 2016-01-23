b10746b36c30131814026fb91b6dcb8b76e8ae3e
jbzdarkid <jbzdarkid@gmail.com>
2016-01-23 04:40:36 -0500

    AI will now target food if against wall & in wrong direction

--- AI.js
+++ AI.js
@@ -4,7 +4,14 @@ var dir = 'right';
 function aiLevel0() {
   if (dir == 'right' || dir == 'left') {
     // Going to the right & blocked by wall
-    if (dir == 'right' && snake[0][0] == width-1) {
+    if (snake[0][0] == food[0]) { // Snake above/below food
+      if (snake[0][1] < food[1]) {
+        dir = 'down';
+      } else {
+        dir = 'up';
+      }
+    // Going right & blocked by wall
+    } else if (dir == 'right' && snake[0][0] == width-1) {
       // If blocked by wall go up, else go down.
       if (snake[0][1] == height-1) {
         dir = 'up';
@@ -19,18 +26,16 @@ function aiLevel0() {
       } else {
         dir = 'up';
       }
-    } else { // Not blocked in given direction, check for food
-      if (snake[0][0] == food[0]) { // Snake above/below food
-        if (snake[0][1] < food[1]) {
-          dir = 'down';
-        } else {
-          dir = 'up';
-        }
-      }
     } // Keep on keeping on
   } else if (dir == 'up' || dir == 'down') {
+    if (snake[0][1] == food[1]) { // Snake left/right of food
+      if (snake[0][0] < food[0]) {
+        dir = 'right';
+      } else {
+        dir = 'left';
+      }
     // Going up & blocked by wall
-    if (dir == 'up' && snake[0][1] === 0) {
+    } else if (dir == 'up' && snake[0][1] === 0) {
       // If blocked by wall go left, else go right.
       if (snake[0][0] == width-1) {
         dir = 'left';
@@ -45,15 +50,7 @@ function aiLevel0() {
       } else {
         dir = 'left';
       }
-    } else { // Not blocked in any direction, check for food
-      if (snake[0][1] == food[1]) { // Snake left/right of food
-        if (snake[0][0] < food[0]) {
-          dir = 'right';
-        } else {
-          dir = 'left';
-        }
-      }
-    }
+    } // Keep on keeping on
   }
   moveHead(dir);
 }
@@ -61,8 +58,14 @@ function aiLevel0() {
 // Same as level 0, but aware of its tail
 function aiLevel1() {
   if (dir == 'right' || dir == 'left') {
+    if (snake[0][0] == food[0]) { // Snake above/below food
+      if (snake[0][1] < food[1]) {
+        dir = 'down';
+      } else {
+        dir = 'up';
+      }
     // Going to the right & blocked by wall | tail
-    if (dir == 'right' && (snake[0][0] == width-1 || inSnake((snake[0][0]+1)+'_'+snake[0][1]))) {
+    } else if (dir == 'right' && (snake[0][0] == width-1 || inSnake((snake[0][0]+1)+'_'+snake[0][1]))) {
       // If blocked by wall | tail go up, else go down.
       if (snake[0][1] == height-1 || inSnake(snake[0][0]+'_'+(snake[0][1]+1))) {
         dir = 'up';
@@ -77,18 +80,16 @@ function aiLevel1() {
       } else {
         dir = 'up';
       }
-    } else { // Not blocked in given direction, check for food
-      if (snake[0][0] == food[0]) { // Snake above/below food
-        if (snake[0][1] < food[1]) {
-          dir = 'down';
-        } else {
-          dir = 'up';
-        }
-      }
     } // Keep on keeping on
   } else if (dir == 'up' || dir == 'down') {
+    if (snake[0][1] == food[1]) { // Snake left/right of food
+      if (snake[0][0] < food[0]) {
+        dir = 'right';
+      } else {
+        dir = 'left';
+      }
     // Going up & blocked by wall | tail
-    if (dir == 'up' && (snake[0][1] === 0 || inSnake(snake[0][0]+'_'+(snake[0][1]-1)))) {
+    } else if (dir == 'up' && (snake[0][1] === 0 || inSnake(snake[0][0]+'_'+(snake[0][1]-1)))) {
       // If blocked by wall | tail go left, else go right.
       if (snake[0][0] == width-1 || inSnake((snake[0][0]+1)+'_'+snake[0][1])) {
         dir = 'left';
@@ -103,15 +104,7 @@ function aiLevel1() {
       } else {
         dir = 'left';
       }
-    } else { // Not blocked in any direction, check for food
-      if (snake[0][1] == food[1]) { // Snake left/right of food
-        if (snake[0][0] < food[0]) {
-          dir = 'right';
-        } else {
-          dir = 'left';
-        }
-      }
-    }
+    } // Keep on keeping on
   }
   moveHead(dir);
 }
