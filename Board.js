1a4dfcda1762adea875b6873e998a0b0c13b4c69
jbzdarkid <jbzdarkid@gmail.com>
2016-01-22 23:49:21 -0500

    Better better debug.

--- Board.js
+++ Board.js
@@ -30,9 +30,11 @@ for (var i=0; i<height; i+=1) {
 function updateBoard(snake) {
   console.log(snake);
   var rows = table.getElementsByTagName("tr"); // Syntax says so.
-  console.log("Rows: "+rows);
+  console.log("Rows");
+  console.log(rows);
 	for (var row in rows) {
-  	console.log("Row: "+row);
+  	console.log("Row:");
+  	console.log(row);
   	var cells = row.getElementsByTagName("td");
 	  for (var cell in cells) {
 	    var isSnakeCell = false;
