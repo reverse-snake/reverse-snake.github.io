b03deedc49668ebc13bf9699e3bcbb11b653064b
jbzdarkid <jbzdarkid@gmail.com>
2016-01-22 23:51:16 -0500

    Using childnodes for row, not rows.

--- Board.js
+++ Board.js
@@ -30,12 +30,9 @@ for (var i=0; i<height; i+=1) {
 function updateBoard(snake) {
   console.log(snake);
   var rows = table.getElementsByTagName("tr"); // Syntax says so.
-  console.log("Rows");
-  console.log(rows);
-  console.log(rows.childNodes);
 	for (var row in rows.childNodes) {
   	console.log("Row:");
-  	console.log(row);
+  	console.log(row.childNodes);
   	var cells = row.getElementsByTagName("td");
 	  for (var cell in cells) {
 	    var isSnakeCell = false;
