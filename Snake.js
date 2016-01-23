f3d0f0d28a5f910cea8fe799d7ceeff7a731933c
Maddy Longo <mclongovt@gmail.com>
2016-01-22 22:17:46 -0500

    Basic move functions complete

--- Snake.js
+++ Snake.js
@@ -3,10 +3,10 @@
 
 // initialize board
 var board = [];     // top left is 0, 0, bottom right is max, max
-var length = 10;  // default values for length and width
-var width = 10;   // will be determined by HTML
-board.length = length;
-for (var i = 0; i < length; i++) {
+var height = 10;  // height is y-coords (also length of board var)
+var width = 10;   // width is x-coords
+board.length = height;
+for (var i = 0; i < height; i++) {
   board[i] = [];
   board[i].length = width;
 }
@@ -14,21 +14,24 @@ for (var i = 0; i < length; i++) {
 // initial values for snake
 var snake = [[1, 1]]; // x, y
 var dir = 'right';
+var food = [-1, -1];
 
 // idle functions (if there are no food bits)
 // go right until right wall is reached, then go down
 function idleRight() {
-  if (snake[0][0] >= board.length - 1) {  // checks if it is at the right wall
-    idleDown();
+  // checks if it is at the right wall or above the food (same x-coords, different y-coords)
+  if (snake[0][0] >= width - 1 || (snake[0][0] == food[0] && snake[0][1] != food[1]) {
+    idleDown(); // this could miss some food appearance, but that is OK because it is stupid
   } else {
     moveHead('right');
     delTail();
+    idleRight();
   }
 }
 
 // go down until collision
 function idleDown() {
-  if (snake[0][1] >= board.length - 1) {  // checks if it is at the bottom wall
+  if (snake[0][1] >= height - 1) {  // checks if it is at the bottom wall
     idleLeft();
   } else {
     moveHead('down');
@@ -36,7 +39,25 @@ function idleDown() {
   }
 }
 
+function idleLeft() {
+  if (snake[0][0] <= 0) {
+    idleUp();
+  } else {
+    moveHead('left');
+    delTail();
+    chooseDir();
+  }
+}
 
+function idleUp() {
+  if (snake[0][1] <= 0) {
+    idleRight();
+  } else {
+    moveHead('up');
+    delTail();
+    chooseDir();
+  }
+}
 
 function moveHead(dir) {
   // move snake's head in given direction
@@ -54,9 +75,11 @@ function moveHead(dir) {
     // some error should happen
   }
   snake.unshift(newHead); // add newHead to the beginning of the list
+  updateBoard(snake);
   // don't remove tail here, because sometimes you may not want to (handled elsewhere)
 }
 
 function delTail() {  // simple function to remove the tail
   snake.pop();
+  updateBoard(snake);
 }
