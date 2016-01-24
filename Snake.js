6fd4ce341a8431e34ca02fbcdabc649d26a62347
jbzdarkid <jbzdarkid@gmail.com>
2016-01-23 20:03:02 -0500

    Implemented impact cells

--- Snake.js
+++ Snake.js
@@ -88,8 +88,8 @@ function moveHead(dir) {
       throw 'cannot move right from ' + newHead[0] + ', ' + newHead[1];
     } else if (inSnake((newHead[0]+1)+'_'+newHead[1])) {
       window.clearInterval(intervalId);
-      // turn head dark red here
-      window.setTimeout(stopGame, 5*refreshRate);
+      window.setTimeout(stopGame, 10*refreshRate
+);
     }
     newHead[0]++;
   } else if (dir == 'left') {
@@ -97,8 +97,8 @@ function moveHead(dir) {
       throw 'cannot move left from ' + newHead[0] + ', ' + newHead[1];
     } else if (inSnake((newHead[0]-1)+'_'+newHead[1])) {
       window.clearInterval(intervalId);
-      // turn head dark red here
-      window.setTimeout(stopGame, 5*refreshRate);
+      window.setTimeout(stopGame, 10*refreshRate
+);
     }
     newHead[0]--;
   } else if (dir == 'up') {  // add new head above current one
@@ -106,8 +106,8 @@ function moveHead(dir) {
       throw 'cannot move up from ' + newHead[0] + ', ' + newHead[1];
     } else if (inSnake(newHead[0]+'_'+(newHead[1]-1))) {
       window.clearInterval(intervalId);
-      // turn head dark red here
-      window.setTimeout(stopGame, 5*refreshRate);
+      window.setTimeout(stopGame, 10*refreshRate
+);
     }
     newHead[1]--;  // changes y-coordinate by -1 (going UP)
   } else if (dir == 'down') {
@@ -115,8 +115,8 @@ function moveHead(dir) {
       throw 'cannot move down from ' + newHead[0] + ', ' + newHead[1];
     } else if (inSnake(newHead[0]+'_'+(newHead[1]+1))) {
       window.clearInterval(intervalId);
-      // turn head dark red here
-      window.setTimeout(stopGame, 5*refreshRate);
+      window.setTimeout(stopGame, 10*refreshRate
+);
     }
     newHead[1]++;
   }
